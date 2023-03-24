3490969efa9d5a9110ff5ac9647cf266817cd538
West <agus2kcoin@outlook.com>
2023-03-24 18:11:38 -0300

    Add text shader

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import './main.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'lil-gui';
 import fragment from './shaders/fragment.glsl.js';
 import vertex from './shaders/vertex.glsl.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
@@ -32,6 +33,7 @@ export default class Sketch {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     this.addMesh();
+    this.settings();
     this.render();
   }
 
@@ -42,6 +44,7 @@ export default class Sketch {
       },
       uniforms: {
         time: { value: 0 },
+        progress: { value: 0 },
       },
       fragmentShader: fragment,
       vertexShader: vertex,
@@ -56,15 +59,191 @@ export default class Sketch {
     // Font
     Promise.all([loadFontAtlas(png)]).then(([atlas]) => {
       const geometry = new MSDFTextGeometry({
-        text: 'HELLO WORLD',
+        text: 'HELLO',
         font: fnt,
       });
 
-      const material = new MSDFTextMaterial();
+      // const material = new MSDFTextMaterial();
+
+      const material = new THREE.ShaderMaterial({
+        side: THREE.DoubleSide,
+        transparent: true,
+        defines: {
+          IS_SMALL: false,
+        },
+        extensions: {
+          derivatives: true,
+        },
+        uniforms: {
+          // Common
+          ...uniforms.common,
+
+          // Rendering
+          ...uniforms.rendering,
+
+          // Strokes
+          ...uniforms.strokes,
+          ...{
+            uStrokeColor: { value: new THREE.Color(0x00ff00) },
+          },
+        },
+        vertexShader: `
+        // Attribute
+        attribute vec2 layoutUv;
+
+        attribute float lineIndex;
+
+        attribute float lineLettersTotal;
+        attribute float lineLetterIndex;
+
+        attribute float lineWordsTotal;
+        attribute float lineWordIndex;
+
+        attribute float wordIndex;
+
+        attribute float letterIndex;
+
+        // Varyings
+        varying vec2 vUv;
+        varying vec2 vLayoutUv;
+        varying vec3 vViewPosition;
+        varying vec3 vNormal;
+
+        varying float vLineIndex;
+
+        varying float vLineLettersTotal;
+        varying float vLineLetterIndex;
+
+        varying float vLineWordsTotal;
+        varying float vLineWordIndex;
+
+        varying float vWordIndex;
+
+        varying float vLetterIndex;
+
+        void main() {
+            // Output
+            vec4 mvPosition = vec4(position, 1.0);
+            mvPosition = modelViewMatrix * mvPosition;
+            gl_Position = projectionMatrix * mvPosition;
+
+            // Varyings
+            vUv = uv;
+            vLayoutUv = layoutUv;
+            vViewPosition = -mvPosition.xyz;
+            vNormal = normal;
+
+            vLineIndex = lineIndex;
+
+            vLineLettersTotal = lineLettersTotal;
+            vLineLetterIndex = lineLetterIndex;
+
+            vLineWordsTotal = lineWordsTotal;
+            vLineWordIndex = lineWordIndex;
+
+            vWordIndex = wordIndex;
+
+            vLetterIndex = letterIndex;
+        }
+    `,
+        fragmentShader: `
+        // Varyings
+        varying vec2 vUv;
+
+        // Uniforms: Common
+        uniform float uOpacity;
+        uniform float uThreshold;
+        uniform float uAlphaTest;
+        uniform vec3 uColor;
+        uniform sampler2D uMap;
+
+        // Uniforms: Strokes
+        uniform vec3 uStrokeColor;
+        uniform float uStrokeOutsetWidth;
+        uniform float uStrokeInsetWidth;
+
+        // Utils: Median
+        float median(float r, float g, float b) {
+            return max(min(r, g), min(max(r, g), b));
+        }
+
+        float rand(float n){return fract(sin(n) * 43758.5453123);}
+
+        float rand(vec2 n) { 
+          return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+        }
+
+        float noise(float p){
+            float fl = floor(p);
+            float fc = fract(p);
+            return mix(rand(fl), rand(fl + 1.0), fc);
+        }
+            
+        float noise(vec2 n) {
+            const vec2 d = vec2(0.0, 1.0);
+            vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+            return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
+        }
+
+        float map(float value, float min1, float max1, float min2, float max2) {
+            return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+        }
+
+        void main() {
+            // Common
+            // Texture sample
+            vec3 s = texture2D(uMap, vUv).rgb;
+
+            // Signed distance
+            float sigDist = median(s.r, s.g, s.b) - 0.5;
+
+            float afwidth = 1.4142135623730951 / 2.0;
+
+            #ifdef IS_SMALL
+                float alpha = smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDist);
+            #else
+                float alpha = clamp(sigDist / fwidth(sigDist) + 0.5, 0.0, 1.0);
+            #endif
+
+            // Strokes
+            // Outset
+            float sigDistOutset = sigDist + uStrokeOutsetWidth * 0.5;
+
+            // Inset
+            float sigDistInset = sigDist - uStrokeInsetWidth * 0.5;
+
+            #ifdef IS_SMALL
+                float outset = smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDistOutset);
+                float inset = 1.0 - smoothstep(uThreshold - afwidth, uThreshold + afwidth, sigDistInset);
+            #else
+                float outset = clamp(sigDistOutset / fwidth(sigDistOutset) + 0.5, 0.0, 1.0);
+                float inset = 1.0 - clamp(sigDistInset / fwidth(sigDistInset) + 0.5, 0.0, 1.0);
+            #endif
+
+            // Border
+            float border = outset * inset;
+
+            // Alpha Test
+            if (alpha < uAlphaTest) discard;
+
+            // Some animation
+            // alpha *= sin(uTime);
+
+            // Output: Common
+
+            vec4 filledFragColor = vec4(uColor, uOpacity * alpha);
+
+            gl_FragColor = filledFragColor;
+            // gl_FragColor = vec4(1., 0. , 0., 1.);
+        }
+    `,
+      });
+
       material.uniforms.uMap.value = atlas;
 
       const mesh = new THREE.Mesh(geometry, material);
-      mesh.scale.set(0.1, 0.1, 0.1);
+      mesh.scale.set(0.02, -0.02, 0.02);
+      mesh.position.x = -1.5;
       this.scene.add(mesh);
     });
 
@@ -85,6 +264,17 @@ export default class Sketch {
 
       return promise;
     }
+  }
+
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new GUI();
+    this.gui.add(this.settings, 'progress', 0, 1, 0.01).onChange(() => {
+      this.material.uniforms.progress.value = this.settings.progress;
+    });
   }
 
   render() {
